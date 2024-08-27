f8cfb615b6f97f50aadbeff0da44463b10272344
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-08-27 13:53:42 -0300

    Adicionando endpoint POST para criar um usuário

--- bff-api/main.py
+++ bff-api/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from settings import DATABASE_NAME
 
+import mongoengine as mongo
+from models.user import User
 
 app = FastAPI()
 
 @app.get('/')
 def _main():
-    return 'Hello World!'
+    mongo.connect(DATABASE_NAME, host='127.0.0.1', port=27017)
+
+    cursor = User.objects()
+    return [c for c in cursor]
+
+
+@app.post('/create_user')
+def _main():
+    mongo.connect(DATABASE_NAME, host='127.0.0.1', port=27017)
+
+    user = User(username='user')
+    user.save()
